08c4dead2dc435dddd2e4fb5bf527e32df5c1577
Daniya Yelemessova <daniya@collingsmail.com>
2024-02-03 19:39:56 -0500

    Add actually form and refactor newTeaForm

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from "prop-types";
 
-function ReusableForm() {
+function ReusableForm(props) {
+
+  const formStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 20,
+    maxWidth: '300px',
+  };
+
+  const buttonStyle = {
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    padding: '10px 15px',
+    border: 'none',
+    cursor: 'pointer'
+  };
   return (
-    <div>ReusableForm</div>
+    <React.Fragment>
+      <div className='container form'>
+      <h1>Fill this form</h1>
+      <form style={formStyle} onSubmit={props.formSubmissionHandler}>
+      <input
+          type='text'
+          name='name'
+          placeholder='Tea Name' required/>
+        <input
+          type="number"
+          name='price'
+          placeholder='Price' required/>
+          <input
+          type="number"
+          name='inStock'
+          placeholder='in Stock' required/>
+        <input
+          type='text'
+          name='type'
+          placeholder='Type of tea' required/>
+        <button style={buttonStyle} type='submit'>{props.buttonText}</button>
+      </form>
+      </div>
+    </React.Fragment>
   )
 }
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
 
 export default ReusableForm
